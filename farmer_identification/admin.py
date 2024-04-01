ec1438964d7903b5b24df32b3c2803f22a3a0be5
Rotse4 <muusyajapheth12@gmail.com>
2024-04-01 23:45:39 +0300

    added admin

--- farmer_identification/admin.py
+++ farmer_identification/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . models import EnumarationGeography, IndividualDetails
 
-# Register your models here.
+class IndividualDetailsAdmin(admin.ModelAdmin):
+    model = IndividualDetails
+    list_display = ('farmer_name', 'national_id', 'email', 'date_of_birth', 'postal', 'mobile_number', 'sex', 'hh_size', 'training')
+
+
+admin.site.register(IndividualDetails, IndividualDetailsAdmin)
