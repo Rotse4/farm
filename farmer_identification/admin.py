91d3480b318000594dcfdee9e9981a7c563bb62d
Rotse4 <muusyajapheth12@gmail.com>
2024-04-03 10:58:29 +0300

    sawa

--- farmer_identification/admin.py
+++ farmer_identification/admin.py
@@ -6,4 +6,11 @@ class IndividualDetailsAdmin(admin.ModelAdmin):
     list_display = ('farmer_name', 'national_id', 'email', 'date_of_birth', 'postal', 'mobile_number', 'sex', 'hh_size', 'training')
 
 
+
+class EnumarationGeographyAdmin(admin.ModelAdmin):
+    model = EnumarationGeography
+    list_display = ('farmer', 'county', 'constituency', 'sub_county', 'ward', 'sub_location', 'village_unit_name', 'enumaration_area_no', 'shopping_center')
+
+
 admin.site.register(IndividualDetails, IndividualDetailsAdmin)
+admin.site.register(EnumarationGeography, EnumarationGeographyAdmin)
