2b946ba7e72c884709a419c7c142ec6a581632d9
Rotse4 <muusyajapheth12@gmail.com>
2024-04-03 16:04:05 +0300

    crop admin added

--- farm_holding/admin.py
+++ farm_holding/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . models import FarmHoldings, Crop
 
 # Register your models here.
+class CropAdmin(admin.ModelAdmin):
+    model = Crop
+    list_display = ('farmer', 'crop', 'total_acrage', 'certified_seeds', 'water_source', 'crop_system', 'fertilizer', 'pestcide')
+
+
+admin.site.register(Crop, CropAdmin)
